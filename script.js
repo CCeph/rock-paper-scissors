90dbbda8ee045ca18d8b3dc33035771d94024361
Yousef <yousefelsisyy@gmail.com>
2022-07-21 17:34:44 -0300

    Function getComputerChoice now generates a random integer from 0 to 2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /**
  * Returns a random computer choice for the game as the string "rock", "paper", or "scissors".
  * 
- * 
+ * var computerNumberChoice stores a random integer from 0 to 2 (inclusive).
  */
 
 function getComputerChoice() {
-    
+    computerNumberChoice = Math.floor(Math.random() * 3);
 }
